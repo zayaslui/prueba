5fd500ad1180f18ed6e485095fcaf7f315542ef0
zayaslui <zayaslui@gmail.com>
2016-06-30 16:27:27 -0400

    udpate

--- rendicion.js
+++ rendicion.js
@@ -1,13 +1,20 @@
 var rendicion=document.getElementById("rendicion");
 var comision=document.getElementById("comision");
-		function calcular(e){
-      console.log("entro"+e.keyCode)
+		function calcular(evt){
+      
+      var k = evt.keyCode ? evt.keyCode : evt.charCode;
+      if(k==13){
+                evt.keyCode = 9; evt.charCode = 9;         
+                evt.preventDefault();
+               }
+      
       if(rendicion.value!=''){
         var result = parseInt(rendicion.value)*0.25;
-        if(e.keyCode==9){
+        if(k===13){
             comision.value=result
         }
       }  
 		}
 
 rendicion.addEventListener("keypress",calcular)
+
